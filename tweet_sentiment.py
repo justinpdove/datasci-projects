1a4b29511c9de75c144847b6e67fa97d65543ebf
justinpdove <justin.p.dove@gmail.com>
2014-02-17 21:36:15 -0500

    Update tweet_sentiment.py
    Added a blurb about this script and cleaned up the commenting.

--- tweet_sentiment.py
+++ tweet_sentiment.py
@@ -1,3 +1,7 @@
+# This is a script that calculates the score of a given tweet using a predefined list of 
+# words and their associated values. That list lives on my local so this script won't do much
+# for you if you want to use it, but it does showcase some of the things I've done with Python.
+
 import sys
 import json
 
@@ -24,26 +28,32 @@ afinnfile = open(sys.argv[1])
 scores = {} # initialize an empty dictionary
 
 for line in afinnfile:
-    term, score = line.split("\t") # The file is tab-delimited. "\t" means "tab character".
-    scores[term] = int(score) # Convert the score to an integer.
-
-# print scores.items() # Print every (term, score) pair in the dictionary.
+    # The file is tab-delimited. "\t" means "tab character".
+    term, score = line.split("\t")
+    # Convert the score to an integer.
+    scores[term] = int(score) 
 
 # initialize an empty list
 data = [] 
 
 for line in open(sys.argv[2]):	
-    data.append(json.loads(line.encode('utf-8'))) # Adding each line of the tweet file into the data list.
+    # Adding each line of the tweet file into the data list.	
+    data.append(json.loads(line.encode('utf-8'))) 
 
 def findTweetScore():
     for i in range(len(data)):
     	tweetScore = 0
-        if 'text' in data[i].keys(): # look for text key in data list item
-            textString = data[i]['text'].encode('utf-8') # set textString variable to tweet string in data list item, encode utf-8 to make it machine readable
-            tweetScore = stringScore(textString) # call stringScore method with tweet string in data list item
-            print float(tweetScore) # print tweet with score
+    	# look for text key in data list item
+        if 'text' in data[i].keys(): 
+            # set textString variable to tweet string in data list item, encode utf-8 to make it machine readable	
+            textString = data[i]['text'].encode('utf-8')
+            # call stringScore method with tweet string in data list item
+            tweetScore = stringScore(textString) 
+            # print tweet with score
+            print float(tweetScore) 
         else:
-        	print float(tweetScore) # print 0.0 because no tweet was found
+            # print 0.0 because no tweet was found
+            print float(tweetScore) 
 
 def stringScore(tweetString):            
 	totalWordScore = 0
